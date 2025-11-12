23b4dc200eddab4746391cc4ed4b64ac44851f53
Suji-12 <23csec23sujithas@gmail.com>
2025-11-12 23:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this._species=species;
-	}
-	get.species(){
-		return this._species;
-	}
-	makeSound() {
+  constructor(species) {
+    this._species = species;
+  }
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
     console.log(`The ${this._species} makes a sound`);
   }
 }
+
 class Dog extends Animal {
   bark() {
     console.log("woof");
